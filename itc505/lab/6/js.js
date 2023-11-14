4b3a4640782cca5c1a5e138a298501108ab7674b
nikhil-edulkanti <144761464+nikhil-edulkanti@users.noreply.github.com>
2023-11-13 21:04:21 -0700

    Add files via upload

---
+++ itc505/lab/6/js.js
@@ -0,0 +1,22 @@
+function redbull(){
+    alert('You chose Redbull')
+    var text = document.getElementById("textField");
+    text.style.display = "block";
+    
+}
+function ferrari(){
+    alert('You chose Ferrari')
+    var text= document.getElementById("ferrariAbt");
+    text.style.display="block";
+}
+
+function mercedes(){
+    alert('You chose Mercedes')
+    var text= document.getElementById("Benz");
+    text.style.display="block";
+    
+
+}
+
+
+
